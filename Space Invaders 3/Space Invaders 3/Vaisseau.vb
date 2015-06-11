aeb4060898437e8da77f7576f011632029168452
François Rousselet <contact@francoisrousselet.fr>
2015-06-11 09:07:34 +0200

    Commentaires vaisseau

--- Space Invaders 3/Space Invaders 3/Vaisseau.vb
+++ Space Invaders 3/Space Invaders 3/Vaisseau.vb
@@ -1,14 +1,18 @@
 ﻿Public Class Vaisseau
-    Inherits Objet
+    Inherits Objet '--> Hérite d'Objet
 
     Dim image As Image
 
+	' Initialisation
     Public Sub New(image As Image)
         MyBase.New(image)
+		' On positionne le vaisseau au milieu
         Me.Location = New Point((FrmJeu.Width / 2) - (Me.Width / 2), FrmJeu.Height - 100)
     End Sub
-
+	
+	' Remise à zéro
     Public Sub reinitialiser()
+	' On positionne le vaisseau au milieu
         Me.Location = New Point((FrmJeu.Width / 2) - (Me.Width / 2), FrmJeu.Height - 100)
     End Sub
 
